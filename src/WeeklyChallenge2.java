f0b955a3efa37a7be340335119c953e7cfb3a85e
mkelly1 <mkelly@his.com>
2019-09-13 14:16:07 -0400

    commit 3

--- src/WeeklyChallenge2.java
+++ src/WeeklyChallenge2.java
@@ -3,22 +3,22 @@ import java.util.*;
 public class WeeklyChallenge2 {
     public static void main(String[] args){
         Random rand = new Random();
-        HashMap<String, String> fixins = new HashMap<>();
-        fixins.put("meat", "chicken,steak,carnidas,chorizo,sofritas,veggies,none,all");
-        fixins.put("rice","brown,white,none,all");
-        fixins.put("salsa", "mild,medium,hot,none,all");
-        fixins.put("beans","pinto,black,none");
-        fixins.put("veggies","lettuce,frajita veggies,none,all");
-        fixins.put("cheese", "cheese");
-        fixins.put("guac","guac");
-        fixins.put("queso","queso");
-        fixins.put("sour_cream","sour_cream");
-        String[] categories = {"meat","rice", "veggies", "cheese", "guac","queso","sour_cream"};
+        HashMap<String, String> fixins = new HashMap<>();            // stores all burrito ingredients
+        fixins.put("meat", "chicken,steak,carnidas,chorizo,sofritas,veggies,none,all"); //0
+        fixins.put("rice","brown,white,none,all");                                      //1
+        fixins.put("salsa", "mild,medium,hot,none,all");                                //2
+        fixins.put("beans","pinto,black,none");                                         //3
+        fixins.put("veggies","lettuce,frajita-veggies,none,all");                       //4
+        fixins.put("cheese","cheese");                                                  //5
+        fixins.put("guac","guac");                                                      //6
+        fixins.put("queso","queso");                                                    //7
+        fixins.put("sour_cream","sour_cream");                                          //8
+        String[] categories = {"meat","rice","salsa","beans","veggies","cheese","guac","queso","sour_cream"};
 
         for (int i=0;i<25;i++) {
             String burrito = "Burrito " + (i+1) + ":";              //String for burrito
             int numOfFixins = rand.nextInt(4) + 6;          // choose random num of ingredients
-            System.out.println("Num of fixins "+numOfFixins);
+            double price = 3 + (.5 * numOfFixins);
             ArrayList<Integer> fixinsSet = new ArrayList<>();   //array to store random categories
             for (int j = 0; j < numOfFixins; j++)
                 while (fixinsSet.size() < numOfFixins) {        //fill array with category nums until size = the random number of categories
@@ -27,26 +27,33 @@ public class WeeklyChallenge2 {
                         fixinsSet.add(randNum);
                     }
                 }
-            System.out.println("Fixins set "+fixinsSet);
-            for (int k = 0; k < numOfFixins; k++){
-                System.out.println("K = "+k);
+            for (int k = 0; k < numOfFixins; k++){                    //construct burrito
                 String category = categories[fixinsSet.get(k)];         // get category by matching fixinsSet number
-                System.out.println("Category "+category);
                 String[] ingredients = fixins.get(category).split(",");
                 int randNum = rand.nextInt(ingredients.length);
                 String ingredient = ingredients[randNum];
-                if(ingredient.equals("none")){
-                    k++;
-                    continue;
+
+                if (ingredient.equals("all")){
+                    burrito = burrito + ", " + ingredients[randNum] + " " +category;
                 }
-                else if (ingredient.equals("all")){
-                    burrito = burrito + "," + ingredients[randNum] + " " +category;
+                else if (ingredient.equals("none")){
+                    burrito = burrito + ", " + "no " + category;
+                }
+                else if (category.equals("beans") && !(ingredient.equals("all"))){
+                    burrito = burrito + ", " + ingredients[randNum] + " " +category;
+                }
+                else if (category.equals("rice") && !(ingredient.equals("all"))) {
+                    burrito = burrito + ", " + ingredients[randNum] + " " + category;
+                }
+                else if (category.equals("salsa") && !(ingredient.equals("all"))) {
+                    burrito = burrito + ", " + ingredients[randNum] + " " + category;
                 }
                 else{
-                    burrito = burrito + "," + ingredients[randNum];
+                    burrito = burrito + ", " + ingredients[randNum];
                 }
             }
-            System.out.println(burrito);
+            System.out.printf(burrito + "\t $%.2f", price);
+            System.out.println("\n");
         }
     }
 }
