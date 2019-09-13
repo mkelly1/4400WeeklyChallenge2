bfe7fad5feb0986159fc2fdc84eb146e0c0f6221
mkelly1 <mkelly@his.com>
2019-09-13 12:00:47 -0400

    Commit 3

--- src/WeeklyChallenge2.java
+++ src/WeeklyChallenge2.java
@@ -1,46 +1,52 @@
-import java.util.HashMap;
-import java.util.Random;
+import java.util.*;
 
 public class WeeklyChallenge2 {
     public static void main(String[] args){
         Random rand = new Random();
-        HashMap<String, String[]> fixins = new HashMap<>();
-        String[] meatIngr = {"chicken", "steak", "carnidas", "chorizo", "sofritas", "veggies", "none", "all"};
-        String[] riceIngr = {"brown", "white", "none", "all"};
-        String[] salsaIngr = {"mild", "medium", "hot", "none", "all"};
-        String[] beansIngr = {"pinto","black","none"};
-        String[] veggiesIngr = {"lettuce", "frajita veggies", "none", "all"};
-        String[] cheese = {""};
-        String[] guac = {""};
-        String[] queso = {""};
-        String[] sour_cream = {""};
+        HashMap<String, String> fixins = new HashMap<>();
+        fixins.put("meat", "chicken,steak,carnidas,chorizo,sofritas,veggies,none,all");
+        fixins.put("rice","brown,white,none,all");
+        fixins.put("salsa", "mild,medium,hot,none,all");
+        fixins.put("beans","pinto,black,none");
+        fixins.put("veggies","lettuce,frajita veggies,none,all");
+        fixins.put("cheese", "cheese");
+        fixins.put("guac","guac");
+        fixins.put("queso","queso");
+        fixins.put("sour_cream","sour_cream");
+        String[] categories = {"meat","rice", "veggies", "cheese", "guac","queso","sour_cream"};
 
-        fixins.put("1", meatIngr );
-        fixins.put("2", riceIngr);
-        fixins.put("3", beansIngr);
-        fixins.put("4", salsaIngr);
-        fixins.put("5", veggiesIngr);
-        fixins.put("6", cheese);
-        fixins.put("7",guac);
-        fixins.put("8",queso);
-        fixins.put("9",sour_cream);
-
-        for (int i=0;i<25=i++){
-
-            String burrito = "Burito "+i;
-            int extras = rand.nextInt(4)+ 5;
-
-            for(int j=0;j < extras; j++){
-                //create an array of 'extras' random numbers
-                int extra = rand.nextInt(9);
-
-                burrito = burrito + ",";
+        for (int i=0;i<25;i++) {
+            String burrito = "Burrito " + (i+1) + ":";              //String for burrito
+            int numOfFixins = rand.nextInt(4) + 6;          // choose random num of ingredients
+            System.out.println("Num of fixins "+numOfFixins);
+            ArrayList<Integer> fixinsSet = new ArrayList<>();   //array to store random categories
+            for (int j = 0; j < numOfFixins; j++)
+                while (fixinsSet.size() < numOfFixins) {        //fill array with category nums until size = the random number of categories
+                    int randNum = rand.nextInt(numOfFixins);    //chose random numbers to put in array
+                    if (!fixinsSet.contains(randNum)) {
+                        fixinsSet.add(randNum);
+                    }
+                }
+            System.out.println("Fixins set "+fixinsSet);
+            for (int k = 0; k < numOfFixins; k++){
+                System.out.println("K = "+k);
+                String category = categories[fixinsSet.get(k)];         // get category by matching fixinsSet number
+                System.out.println("Category "+category);
+                String[] ingredients = fixins.get(category).split(",");
+                int randNum = rand.nextInt(ingredients.length);
+                String ingredient = ingredients[randNum];
+                if(ingredient.equals("none")){
+                    k++;
+                    continue;
+                }
+                else if (ingredient.equals("all")){
+                    burrito = burrito + "," + ingredients[randNum] + " " +category;
+                }
+                else{
+                    burrito = burrito + "," + ingredients[randNum];
+                }
             }
-
-
+            System.out.println(burrito);
         }
-
-
-
     }
 }
